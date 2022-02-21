6c39990142d70236ce7d43053869f3967c38576e
Abhishek Sirohi <asirohi@qualys.com>
2022-02-21 21:25:29 +0530

    chore: plan and subscription

--- src/controllers/users/profile.ts
+++ src/controllers/users/profile.ts
@@ -1,7 +1,7 @@
 import { Response, NextFunction } from 'express';
 
 import { getDb } from 'database/connect';
-import { SubscriptionModel, UserModel } from 'models';
+import { PlanModel, SubscriptionModel, UserModel } from 'models';
 import { AuthenticatedRequest } from 'types/JwtPayload';
 
 export const profile = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
@@ -9,11 +9,19 @@ export const profile = async (req: AuthenticatedRequest, res: Response, next: Ne
 
   const userModel = new UserModel(getDb());
   const subscriptionModel = new SubscriptionModel(getDb());
+  const planModel = new PlanModel(getDb());
 
   try {
     const user = await userModel.get(_id);
     const subscription = await subscriptionModel.find({ user: _id });
-    return res.status(200).json({ ...user, password: undefined, subscription: subscription[0] });
+
+    let userSubscription = null;
+    if (subscription.length) {
+      userSubscription = subscription[0];
+      userSubscription.plan = await planModel.get(userSubscription.plan);
+    }
+
+    return res.status(200).json({ ...user, password: undefined, subscription: userSubscription });
   } catch (err) {
     return res.status(500).send({ error: err });
   }
