a3303959d500f0516da78b9b6da0c7bba9a239cb
Abhishek Sirohi <asirohi@qualys.com>
2022-04-24 18:52:42 +0530

    chore: auth fix

--- src/controllers/auth/token.ts
+++ src/controllers/auth/token.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import { getDb } from 'database/connect';
 import User from 'models/user';
@@ -11,18 +11,21 @@ export const getAccessToken = async (req: Request, res: Response, next: NextFunc
   const userModel = new User(getDb());
 
   try {
-    const email = hashGenerator.validateHashForUser(refresh_token as string);
+    const user = hashGenerator.validateHashForUser(refresh_token as string);
 
-    console.log(email);
-    if (!email) {
+    if (!user) {
       return res.status(401).json({ code: 900, message: 'Refresh token expired. Login again' });
     }
-    const [userData] = await userModel.find({ email }, 1);
+
+    const isMobile = /^\+91[0-9]{10}$/.test(user);
+    const q = isMobile ? { mobile: user } : { email: user };
+
+    const [userData] = await userModel.find(q, 1);
     if (!userData) {
       return res.json(400).json({ code: 901, message: 'Invalid email' });
     }
-    const { role, _id, name, created_at } = userData;
-    const access_token = createJwtToken({ email, _id: _id.toHexString(), name, created_at, role });
+    const { role, _id, name, created_at, email, mobile } = userData;
+    const access_token = createJwtToken({ email, mobile, _id: _id.toHexString(), name, created_at, role });
     refresh_token = hashGenerator.generateForUser(email, 24);
     return res.status(200).json({ access_token, refresh_token });
   } catch (err) {
